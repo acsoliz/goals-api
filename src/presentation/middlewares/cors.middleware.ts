6ef00d4e53cfbb04b6dea2ee07ab9e2757f874e5
acsoliz <acsoliz@outlook.com>
2024-04-18 22:07:56 +0200

    added localhost to cors permision

--- src/presentation/middlewares/cors.middleware.ts
+++ src/presentation/middlewares/cors.middleware.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 export class CorsMiddleware {
   static configure() {
     return cors({
-      origin: '*',
+      origin: ['*', 'http://localhost:3000'], // Permitir solicitudes de estos orígenes
       methods: ['GET', 'POST', 'PUT', 'DELETE'], // Permitir los métodos HTTP especificados
       allowedHeaders: ['Content-Type', 'Authorization'], // Permitir los encabezados especificados
       optionsSuccessStatus: 200 // Establecer el código de estado para las solicitudes preflight OPTIONS
