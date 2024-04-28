056a5a9dae2063742d84f423baef3edffee24473
acsoliz <acsoliz@outlook.com>
2024-04-28 02:23:20 +0200

    origin added in cors midleware

--- src/presentation/middlewares/cors.middleware.ts
+++ src/presentation/middlewares/cors.middleware.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 export class CorsMiddleware {
   static configure() {
     return cors({
-      origin: ['*', 'http://localhost:19006'], // Permitir solicitudes de estos orígenes
+      origin: '*', // Permitir solicitudes de estos orígenes
       methods: ['GET', 'POST', 'PUT', 'DELETE'], // Permitir los métodos HTTP especificados
       allowedHeaders: ['Content-Type', 'Authorization'], // Permitir los encabezados especificados
       optionsSuccessStatus: 200 // Establecer el código de estado para las solicitudes preflight OPTIONS
