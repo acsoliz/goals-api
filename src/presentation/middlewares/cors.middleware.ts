eedc48cf2ddceccacb13efe9aeaf13b5537c19cf
acsoliz <acsoliz@outlook.com>
2024-04-29 15:55:57 +0200

    fixing cors error

--- src/presentation/middlewares/cors.middleware.ts
+++ src/presentation/middlewares/cors.middleware.ts
@@ -5,7 +5,7 @@ export class CorsMiddleware {
   static configure() {
     return cors({
       origin: '*', // Permitir solicitudes de estos orígenes
-      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Permitir los métodos HTTP especificados
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Permitir los métodos HTTP especificados
       allowedHeaders: ['Content-Type', 'Authorization'], // Permitir los encabezados especificados
       optionsSuccessStatus: 200 // Establecer el código de estado para las solicitudes preflight OPTIONS
     });
